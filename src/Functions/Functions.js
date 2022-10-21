efa2fa5968d92603f082878a5241c63a87b8a987
EemeliAntikainen <73908127+EemeliAntikainen@users.noreply.github.com>
2022-10-21 23:01:59 +0300

    tein vähän logiikkaa

--- src/Functions/Functions.js
+++ src/Functions/Functions.js
@@ -1,13 +1,39 @@
-import React, { useEffect,useState } from 'react';
 
-const MorseTable = [{morse:"1", alphabet:'E'}]
+
 const MorseConverter = (e)=>{
+    var MorseTable = [
+        {morse:"·-", alphabet:'A'},
+        {morse:"-", alphabet:'E'}
+    ]
+    // Saadakseen tietää kumittiko käyttäjä kirjaimen, otetaan kirjainhistoria 2 kirjainta taaksepäin ja verrataan sitö nykyiseen inputtiin funktiossa.
+
+    var Text = e.Text;
+    var Letter = [Text.length-1]
+    console.log(e);
+    
     if(e.Morse == true){
-        return e.Text;
+
+        MorseTable.map((i,l) => {
+            if(i.alphabet ==  Letter?.toUpperCase()){
+                Text.push(i.morse)
+                console.log(Text);
+                
+            }
+        });
+
+        return Text;
 
     }
     else if(e.Morse == false){
 
+        MorseTable.map(i => {
+            if(i.morse == e.Morse){
+                Text[0] = Text[0]+i.alphabet
+                console.log(Text);
+            }
+        });
+
+        return Text;
     }
     else {
         return"Fatal Error"
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,31 @@
-import React, { useEffect,useState } from 'react';
+import  { useEffect,useState } from 'react';
 import {MorseConverter} from '../Functions/Functions'
 
 const Home = ()=>{
+    const [Output, setOutput] = useState([]);
     const [Text, setText] = useState("");
+    
+    useEffect(()=>{
+        setOutput(MorseConverter({ Morse:true,Text:Text }))
+    },[Text]);
+
+    const textToElement = Output.map((e,i)=>{
+        return " "+ e
+    });
+
     return(
         <div>
             <h1>Morsetus</h1>
-            <h4>Morse to text</h4>
-            <p>{Text}</p>
-            <input type="textbox" onChange={(e)=>{setText(MorseConverter({ Morse:true,Text:e.target.value}))}}/>
+            <h4>Morse to text</h4> 
+
+            <div>
+                {textToElement}
+            </div>
+            <input type="textbox" onChange={(e)=>{
+                    setText(e.target.value);
+                    console.log(Output);
+                }
+                }/>
         </div>
     )
 }
